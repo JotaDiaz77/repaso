ee138e1ba62ea4b5aeae23e895f6fb82b020eb80
Jota Díaz <josmondia2@alu.edu.gva.es>
2023-12-31 19:12:47 +0100

    31/12/23

--- src/ejerciciosPOO/COrdenador.java
+++ src/ejerciciosPOO/COrdenador.java
@@ -1,2 +1,40 @@
-package ejerciciosPOO;public class COrdenador {
+package ejerciciosPOO;
+
+public class COrdenador {
+    String marca;
+    String procesador;
+    String pantalla;
+    boolean ordenadorEncendido;
+
+    void encenderOrdenador(){
+        if (ordenadorEncendido){
+            System.out.println("El ordenador ya está encendido");
+        }else{
+            ordenadorEncendido = true;
+            System.out.println("El ordenador se está encendiendo");
+        }
+    }
+
+    void apagarOrdenador(){
+        if (!ordenadorEncendido){
+            System.out.println("El ordenador ya está apagado");
+        }else{
+            ordenadorEncendido = false;
+            System.out.println("El ordenador se está apagando");
+        }
+    }
+
+    public void estado(){
+        System.out.println("Datos del ordenador: " +
+                "Marca -> " +this.marca
+                +" | Procesador -> "+this.procesador
+                +" | Pantalla -> "+this.pantalla);
+
+        if (ordenadorEncendido){
+            System.out.println("El ordenador está encendido");
+        }else{
+            System.out.println("El ordenador está apagado");
+        }
+    }
+
 }
--- src/ejerciciosPOO/COrdenadorApp.java
+++ src/ejerciciosPOO/COrdenadorApp.java
@@ -1,2 +1,15 @@
-package ejerciciosPOO;public class COrdenadorApp {
+package ejerciciosPOO;
+
+public class COrdenadorApp {
+    public static void main(String[] args) {
+        COrdenador c1 = new COrdenador();
+
+        c1.marca = "Ast";
+        c1.procesador = "AMD";
+        c1.pantalla = "TFT";
+        c1.ordenadorEncendido = false;
+
+        c1.apagarOrdenador();
+        c1.estado();
+    }
 }
